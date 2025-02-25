4ee3ee23a90162d16e7243e61b724f326b4bd5f8
aashiro1 <shakyaaashish@gmail.com>
2025-02-25 09:37:26 +0545

    python intro and datatypes

---
+++ 2025-02-14/python intro/datatypes.py
@@ -0,0 +1,24 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+print("Hello, World!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+mylist = ['a','b','c','d','e']
+listtwo = ['x','y','z']
+mylist.extend(listtwo)
+print(mylist)
